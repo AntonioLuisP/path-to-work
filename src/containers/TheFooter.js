6534d9d6ecaeabd3b6473ddc9d4911ace53f5268
Antônio Luis <antonioluisp97@gmail.com>
2021-06-01 20:40:26 -0300

    arrumando layout com o redux footer

--- src/containers/TheFooter.js
+++ src/containers/TheFooter.js
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import { CFooter } from '@coreui/react'
 
 const TheFooter = () => {
@@ -6,7 +7,7 @@ const TheFooter = () => {
     <CFooter fixed={false}>
       <div>
         <a href="https://github.com/AntonioLuisP" target="_blank" rel="noopener noreferrer">Note Recomendation</a>
-        {/* <span className="ml-1">&copy; 2020 creativeLabs.</span> */}
+
       </div>
       <div className="mfs-auto">
         <span className="mr-1">Feito por:</span>
