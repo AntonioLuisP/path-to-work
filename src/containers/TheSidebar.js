929d1548086c4824e56f3958b39aee9d2ee8fa2a
Antônio Luis <antonioluisp97@gmail.com>
2021-06-01 20:39:28 -0300

    arrumando layout com o redux sidebar

--- src/containers/TheSidebar.js
+++ src/containers/TheSidebar.js
@@ -1,62 +1,63 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+
 import {
-  CCreateElement,
   CSidebar,
   CSidebarBrand,
   CSidebarNav,
-  CSidebarNavDivider,
   CSidebarNavTitle,
   CSidebarMinimizer,
-  CSidebarNavDropdown,
   CSidebarNavItem,
 } from '@coreui/react'
 
 import CIcon from '@coreui/icons-react'
 
 import {
-  cilNotes,
+  cilCursor,
+  cilHome,
+  cilList,
 } from '@coreui/icons'
 
-// sidebar nav config
-import navigation from './_nav'
+
 import Items from './Items'
 
 const TheSidebar = () => {
+
   const dispatch = useDispatch()
   const show = useSelector(state => state.sidebarShow)
-  const projects = useSelector(state => state.projects)
 
   return (
     <CSidebar
       show={show}
       onShowChange={(val) => dispatch({ type: 'set', sidebarShow: val })}
     >
-      <CSidebarBrand className="d-md-down-none" to="/">
-        <CIcon
-          className="c-sidebar-brand-full"
-          name="logo-negative"
-          content={cilNotes}
-          height={35}
-        />
-        <CIcon
-          className="c-sidebar-brand-minimized"
-          name="sygnet"
-          content={cilNotes}
-          height={35}
-        />
+      <CSidebarBrand className="d-md-down-none text-center" >
+        Path Work
       </CSidebarBrand>
       <CSidebarNav>
-        <CCreateElement
-          items={navigation}
-          components={{
-            CSidebarNavDivider,
-            CSidebarNavDropdown,
-            CSidebarNavItem,
-            CSidebarNavTitle
-          }}
-        />      
-      <Items />
+        <CSidebarNavItem
+          name="Dashboard"
+          to="/dashboard"
+          icon={<CIcon content={cilHome} customClasses="c-sidebar-nav-icon" />}
+        />
+        <CSidebarNavTitle>Link</CSidebarNavTitle>
+        <CSidebarNavItem
+          name="Links"
+          to="/links"
+          icon={<CIcon content={cilCursor} customClasses="c-sidebar-nav-icon" />}
+        />
+        <CSidebarNavItem
+          name="Social"
+          to="/social"
+          icon={<CIcon content={cilCursor} customClasses="c-sidebar-nav-icon" />}
+        />
+        <CSidebarNavTitle>Projetos</CSidebarNavTitle>
+        <CSidebarNavItem
+          name="Todos Projetos"
+          to="/projects"
+          icon={<CIcon content={cilList} customClasses="c-sidebar-nav-icon" />}
+        />
+        <Items />
       </CSidebarNav>
       <CSidebarMinimizer className="c-d-md-down-none" />
     </CSidebar>
