067a827eaf56e6362012c3fdd0a701b6a504f444
Antônio Luis <antonioluisp97@gmail.com>
2021-07-12 03:35:06 -0300

    home view ok

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -7,7 +7,6 @@ import {
   CButton,
   CCard,
   CCardBody,
-  CCardGroup,
   CCol,
   CContainer,
   CForm,
@@ -44,54 +43,46 @@ const Login = () => {
     <div className="c-app c-default-layout flex-row align-items-center">
       <CContainer>
         <CRow className="justify-content-center">
-          <CCol md="8">
-            <CCardGroup>
-              <CCard className="p-4">
-                <CCardBody>
-                  <CForm onSubmit={handleLogin}>
-                    <h1>Login</h1>
-                    <p className="text-muted">Entre com sua conta</p>
-                    <CInputGroup className="mb-3">
-                      <CInputGroupPrepend>
-                        <CInputGroupText>
-                          <CIcon name="cil-user" />
-                        </CInputGroupText>
-                      </CInputGroupPrepend>
-                      <CInput type="text" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} autoComplete="email" />
-                    </CInputGroup>
-                    <CInputGroup className="mb-4">
-                      <CInputGroupPrepend>
-                        <CInputGroupText>
-                          <CIcon name="cil-lock-locked" />
-                        </CInputGroupText>
-                      </CInputGroupPrepend>
-                      <CInput type="password" placeholder="Senha" value={password} onChange={e => setPassword(e.target.value)} autoComplete=" password" />
-                    </CInputGroup>
-                    <CRow>
-                      <CCol xs="6">
-                        <CButton type='submit' color="primary" className="px-4">Login</CButton>
-                      </CCol>
-                      <CCol xs="6" className="text-right">
-                        <OAuth />
-                      </CCol>
-                    </CRow>
-                  </CForm>
-                </CCardBody>
-              </CCard>
-              <CCard className="text-white bg-gradient-info py-5 d-md-down-none" style={{ width: '44%' }}>
-                <CCardBody className="text-center">
-                  <div>
-                    <h2>Cadastre-se</h2>
-                    <p className="text-justify">
-                      Nesse sistema você pode gerenciar todos os links importantes de sua vida. Dentre as possibilidades estão <b>listagem, anotações e criação de tarefas para esses links</b>. Aqui também você poderá criar um perfil compartilhável com todos os seus <b>links sociais</b> para as pessoas conseguirem te encontrar!!!
-                    </p>
-                    <Link to="/register" className='text-black-50'>
-                      Registre-se agora!
-                    </Link>
-                  </div>
-                </CCardBody>
-              </CCard>
-            </CCardGroup>
+          <CCol md="6">
+            <CCard className="p-4">
+              <CCardBody>
+                <CForm onSubmit={handleLogin}>
+                  <h1>Login</h1>
+                  <p className="text-muted">Entre com sua conta</p>
+                  <CInputGroup className="mb-3">
+                    <CInputGroupPrepend>
+                      <CInputGroupText>
+                        <CIcon name="cil-user" />
+                      </CInputGroupText>
+                    </CInputGroupPrepend>
+                    <CInput type="text" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} autoComplete="email" />
+                  </CInputGroup>
+                  <CInputGroup className="mb-4">
+                    <CInputGroupPrepend>
+                      <CInputGroupText>
+                        <CIcon name="cil-lock-locked" />
+                      </CInputGroupText>
+                    </CInputGroupPrepend>
+                    <CInput type="password" placeholder="Senha" value={password} onChange={e => setPassword(e.target.value)} autoComplete=" password" />
+                  </CInputGroup>
+                  <CRow>
+                    <CCol xs="6">
+                      <CButton type='submit' color="primary" className="px-4">Login</CButton>
+                    </CCol>
+                    <CCol xs="6" className="text-right">
+                      <OAuth />
+                    </CCol>
+                  </CRow>
+                  <CRow>
+                    <CCol xs="12" className="text-right">
+                      <Link to="/register">
+                        Não possui conta? Registre-se agora!
+                      </Link>
+                    </CCol>
+                  </CRow>
+                </CForm>
+              </CCardBody>
+            </CCard>
           </CCol>
         </CRow>
       </CContainer>
--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -7,11 +7,15 @@ import {
 } from '../../containers'
 
 import {
+    CRow,
+    CLink,
+    CCol,
     CContainer,
     CHeader,
     CHeaderNav,
     CHeaderNavItem,
-    CHeaderNavLink
+    CHeaderNavLink,
+    CJumbotron,
 } from '@coreui/react'
 
 export default function Home() {
@@ -19,12 +23,12 @@ export default function Home() {
     const { authUser } = useAuth()
 
     return (
-        <div className="c-app c-default-layout">
+        <div className="c-app c-default-layout flex-row a align-items-center">
             <div className="c-wrapper">
                 <CHeader>
                     <CHeaderNav className="d-md-down-none mr-auto">
                         <CHeaderNavItem className="px-3" >
-                            <CHeaderNavLink to="/home">Link Work</CHeaderNavLink>
+                            <CHeaderNavLink to="/dashboard">Link Work</CHeaderNavLink>
                         </CHeaderNavItem>
                     </CHeaderNav>
                     {authUser ?
@@ -36,9 +40,30 @@ export default function Home() {
                         </CHeaderNav>
                     }
                 </CHeader>
-                <div className="c-body">
-                    <CContainer>
-                        HOME
+                <div className="c-body p-4">
+                    <CContainer fluid>
+                        <CRow className="justify-content-center">
+                            <CCol md="7">
+                                <CJumbotron className="border text-white bg-gradient-info">
+                                    <h1 className="display-3">Bem vindo ao Link Work!</h1>
+                                    <p className="lead">Este é um sistema gestão de links feito para você! Aqui você pode gerenciar todos os links importantes de sua vida.</p>
+                                    <hr className="my-2" />
+                                    <p className="text-justify">
+                                        Dentre as possibilidades estão <b>listagem, anotações e criação de tarefas para esses links</b>. Aqui também você poderá criar um perfil compartilhável com todos os seus <b>links sociais</b> para as pessoas conseguirem te encontrar!
+                                    </p>
+                                    <p className="lead">
+                                        <CLink
+                                            to='/dashboard'
+                                            size='lg'
+                                            className='btn btn-primary bg-gradient-primary'
+                                            rel="noreferrer noopener"
+                                        >
+                                            Venha para seu Dashboard
+                                        </CLink>
+                                    </p>
+                                </CJumbotron>
+                            </CCol>
+                        </CRow>
                     </CContainer>
                 </div>
                 <Footer />
